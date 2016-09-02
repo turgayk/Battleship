18a59ec40800dcb8e1de9cf531fcb8a7283ff87a
mdsimmo <mdsimmo2+git@gmail.com>
2016-09-02 16:27:19 +1000

    removed some bad imports

--- src/Model/Player.cs
+++ src/Model/Player.cs
@@ -3,8 +3,7 @@ using Microsoft.VisualBasic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics;
+
 /// <summary>
 /// Player has its own _PlayerGrid, and can see an _EnemyGrid, it can also check if
 /// all ships are deployed and if all ships are detroyed. A Player can also attach.
--- src/UtilityFunctions.cs
+++ src/UtilityFunctions.cs
@@ -3,8 +3,6 @@ using Microsoft.VisualBasic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics;
 using SwinGameSDK;
 /// <summary>
 /// This includes a number of utility methods for
