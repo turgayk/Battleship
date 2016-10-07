2c4d9fe68de395931d131731eb8031d977d94d02
LuckyTing123 <304192040@qq.com>
2016-10-07 15:56:38 +1100

    PlayerUnitTest.cs

--- src/tests/PlayerUnitTest.cs
+++ src/tests/PlayerUnitTest.cs
@@ -1,23 +1,21 @@
 using NUnit.Framework;
 using SwinGameSDK;
 
-[TestFixture]
-public class BattleshipPlayTest
-{
-	[Test()]
-	public void AlignDeployment(){
-		BattlesipGame b= new BattleshipGame();
-		Player player = new player(b);
-
-		Ship tug = player.Ship(ShipName.Tug);
-		Ship submarine = player.Ship(ShipName.Submarine);
-		Ship destroyer = player.Ship(ShipName.Destroyer);
-		Ship battleship = player.Ship(ShipName.Battleship);
-		Ship aircrafCarrier = player.Ship(ShipName.AircrafCarrier);
-		Assert.IsTrue(tug.Column == submarin.Column && submarine.Colume == destroyer.Column && destroyer.Column == battleship.Column && battleship.Column == aircraftCarrier.Column );
-		)
-
-
-
+[TestFixuture()]
+	public class BattleshipPlayATest
+	{
+		[Test()]
+		publi void AlignDeployment(){
+			
+			BattleShipsGame b = new BattleShipsGame();
+			Player player = new Player(b);
+			
+			Ship tug = player.Ship(ShipName.Tug);
+			Ship submarine = player.Ship(ShipName.Submarine);
+			Ship destroyer = player.Ship(ShipName.Destroyer);
+			Ship battleship = player.Ship(ShipName.Battleship);
+			Ship aircraftCarrier = player.Ship(ShipName.AircraftCarrier);
+			Assert.IsTrue(tug.Column == submarine.Column && submarine.Column == destroyer.Column && destroyer.Column == battleship.Column && battleship.Column == aircraftCarrier.Column  );
+			}
+		}
 	}
- }
