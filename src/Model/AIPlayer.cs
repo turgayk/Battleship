865542c88bcf3e8441e3080e907a34def6b881bb
Michael Pieniazek <Michael Pieniazek>
2016-10-15 14:59:07 +1100

    refixed delay, its still but a lot shorter

--- src/Model/AIPlayer.cs
+++ src/Model/AIPlayer.cs
@@ -122,7 +122,7 @@ public abstract class AIPlayer : Player
 	private void Delay()
 	{
 		int i = 0;
-		for (i = 0; i <= 150; i++) {
+		for (i = 0; i <= 50; i++) {
 			//Dont delay if window is closed
 			if (SwinGame.WindowCloseRequested())
 				return;
